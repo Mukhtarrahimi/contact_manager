b30ffefe85d6866cb4092a8fa623d1ec84d4a2a0
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-02 09:44:51 +0430

    add function for contact_list

--- contact_manager.py
+++ contact_manager.py
@@ -2,3 +2,7 @@ class ContactManager:
     def __init__(self):
         self.contact_list = []
         
+    def add(self,id, name, phone_number):
+        self.contact_list.append({"id":id,
+                                  "name" :name,
+                                  "phone_number" : phone_number})
