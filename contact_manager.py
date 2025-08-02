dc6f60dab4be0d08b570c9e3a7efa2f23b6b23af
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-02 10:10:52 +0430

    function for display & and create object as class.

--- contact_manager.py
+++ contact_manager.py
@@ -10,10 +10,29 @@ class ContactManager:
                                   "phone_number" : phone_number})
         
     # function for search in contact list
-    def search(self, name, phone_number):
+    def search(self, name= None, phone_number= None):
         result = []
-        for contact in enumerate(self.contact_list):
-            if contact["name"] == name or contact["phone_number"] == phone_number:
+        for contact in self.contact_list:
+            if contact["name"].lower() == name or contact["phone_number"] == phone_number:
                 result.append(contact)
                 
-        print(result)
+        if result:
+            print("result search:")
+            for c in result:
+                print(f"Name: {c['name']}\nphone: {c['phone_number']}")
+        else:
+            print("no result search")
+        
+    def display(self):
+        print("Contact list")
+        for c in self.contact_list:
+            print(f"{c['id']}. Name: {c['name']}\n   phone: {c['phone_number']}")
+            
+            
+        
+con = ContactManager()
+con.add(1, "John", "123456")
+con.add(2, "rahimi", "789012")
+con.search(name = "rahimi")
+con.display()
+
