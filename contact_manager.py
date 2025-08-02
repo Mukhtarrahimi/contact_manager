1cdfdaa72afe4b4630248cc2ba8df630d4a746dd
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-02 09:56:26 +0430

    function for search in contact_list

--- contact_manager.py
+++ contact_manager.py
@@ -2,7 +2,18 @@ class ContactManager:
     def __init__(self):
         self.contact_list = []
         
+
+    # function for add contact
     def add(self,id, name, phone_number):
         self.contact_list.append({"id":id,
                                   "name" :name,
                                   "phone_number" : phone_number})
+        
+    # function for search in contact list
+    def search(self, name, phone_number):
+        result = []
+        for contact in enumerate(self.contact_list):
+            if contact["name"] == name or contact["phone_number"] == phone_number:
+                result.append(contact)
+                
+        print(result)
